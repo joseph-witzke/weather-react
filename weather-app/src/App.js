5cb09fe4b831d8f771406030c2e66e9fb752f141
Joe Witzke <joewitzke@joes-mbp.lan>
2021-09-17 09:45:16 -0600

    writes datebuilder function and displays current date on home page

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,3 +1,6 @@
+//IMPORTS
+import React, { useState } from 'react';
+
 //STYLES
 import './App.css';
 
@@ -6,10 +9,46 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
+  const [weather, setWeather] = useState({});
+
+  const dateBuilder = (item) => {
+    let months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+    let days = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ];
+
+    let day = days[item.getDay()];
+    let date = item.getDate();
+    let month = months[item.getMonth()];
+    let year = item.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+
   return (
     <div className='App'>
       <Header />
       <h1>Weather App</h1>
+      <h2>{dateBuilder(new Date())}</h2>
       <Footer />
     </div>
   );
